5cba563ceddccbc152fbc094a08e3ef1b30412fb
Marvin Borner <git@marvinborner.de>
2024-04-25 15:16:52 +0200

    Add casestudies to bechmarked files

--- generate/benchmarks.sh
+++ generate/benchmarks.sh
@@ -1,5 +1,10 @@
 #!/bin/env bash
 set -e
 
+# we need to cd here since some examples import local files
+cd ../effekt/
+
+files="examples/benchmarks/*.effekt examples/casestudies/*.effekt.*"
+
 # run benchmarks and gather as much information as possible
-$(which time) --verbose effekt.sh --time json ../effekt/examples/benchmarks/*.effekt >/dev/null 2>time.out
+$(which time) --verbose effekt.sh --time json $files >/dev/null 2>../generate/time.out
