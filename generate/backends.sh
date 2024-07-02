563a1d4ccc7a08b50e21d27e8e7ea8711bd7ced0
Marvin Borner <git@marvinborner.de>
2024-07-02 14:03:55 +0200

    Actually use new benchmarking framework

--- generate/backends.sh
+++ generate/backends.sh
@@ -3,38 +3,16 @@ set -e
 
 BACKENDS="llvm js"
 
-combineMetrics() {
-	TIMEFILE="$1"
-
-	maxMem=$(grep "Maximum resident set size" "$TIMEFILE" | awk '{print $6}')
-	userTime=$(grep "User time" "$TIMEFILE" | awk '{print $4}')
-	cpuUsage=$(grep "Percent of CPU this job got" "$TIMEFILE" | awk '{print $7}')
-
-	jq -n \
-		--arg maxMem "$maxMem" \
-		--arg userTime "$userTime" \
-		--arg cpuUsage "$cpuUsage" \
-		--arg backend "$backend" \
-		'{maxMem: $maxMem, userTime: $userTime, cpuUsage: $cpuUsage}'
-
-	# rm "$TIMEFILE"
-}
-
 {
 	for backend in $BACKENDS; do
 		echo "{\"$backend\":"
 		{
-			for file in $(find ../effekt/ -name "*.time-$backend.out"); do
-				exitStatus=$(grep "Exit status" "$file" | awk '{print $3}')
-				if [ "$exitStatus" -ne 0 ]; then
-					continue
-				fi
-				_file="${file//"../effekt/"/}"
-				_file="${_file//".time-$backend.out"/}"
-				echo "{\"$_file\":"
-				combineMetrics "$file"
-				echo "}"
-			done
+			log="../effekt/benchmarks_$backend.log"
+
+			while read data; do
+				arr=($data)
+				echo "{\"${arr[0]}\": {\"time\": ${arr[2]}, \"arg\": ${arr[1]}}}"
+			done <"$log"
 		} | jq -s 'add'
 		echo "}"
 	done
--- generate/benchmarks.sh
+++ generate/benchmarks.sh
@@ -7,12 +7,12 @@ cd ../effekt/
 FILES="examples/benchmarks/*.effekt examples/casestudies/*.effekt.md"
 
 # measure build performance first
-# for file in $FILES; do
-# 	"$(which time)" --verbose effekt.sh -o buildout/ -b "$file" >/dev/null 2>"$file".time.out
-# done
+for file in $FILES; do
+	"$(which time)" --verbose effekt.sh -o buildout/ -b "$file" >/dev/null 2>"$file".time.out
+done
 
 # then build and run all with JSON phase timings
-# "$(which time)" --verbose effekt.sh -o out/ --time json $FILES >/dev/null 2>../generate/time.out
+"$(which time)" --verbose effekt.sh -o out/ --time json $FILES >/dev/null 2>../generate/time.out
 
 # now also measure execution time of backends based on benchmark configuration
 BACKENDS="llvm js"
@@ -21,21 +21,10 @@ for backend in $BACKENDS; do
 	log="benchmarks_$backend.log"
 	>"$log"
 	while read config; do
+		echo "$config"
 		arr=($config)
 		file="examples/benchmarks/${arr[0]}/${arr[1]}.effekt"
-		effekt.sh --backend "$backend" "$file" -- ${arr[2]} >>$log
+		printf "${arr[0]}/${arr[1]} ${arr[2]} " >>$log
+		time effekt.sh --backend "$backend" "$file" -- ${arr[2]} >>$log
 	done <../generate/benchmark_config.txt
 done
-
-# for backend in $BACKENDS; do
-# 	for file in $FILES; do
-# 		effekt.sh --backend "$backend" -b "$file" &>/dev/null || true
-# 		base=$(basename "$file")
-# 		path=$(find ./out/ -type f -name "*${base%%.*}") # issue #471
-# 		if [ -z "$path" ]; then
-# 			echo "$file failed to build for $backend."
-# 			continue
-# 		fi
-# 		"$(which time)" --verbose "$path" >/dev/null 2>"$file".time-"$backend".out || echo "$path failed to run for $backend."
-# 	done
-# done
