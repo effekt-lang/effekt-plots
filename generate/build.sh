c32a65decda5ab1975442b348d42902aba40dd05
Marvin Borner <git@marvinborner.de>
2024-12-22 15:25:59 +0100

    Output build for debugging

--- generate/build.sh
+++ generate/build.sh
@@ -5,6 +5,8 @@ set -x
 cd ../effekt/
 git submodule update --init --recursive
 
+>&2 sbt install
+
 time=$({ $(which time) -f '%e' sbt install >/dev/null; } 2>&1 | tail -n1)
 jq -n \
 	--arg buildTime "$time" \
