51b52d952fdcf641a606ee7d1df684353b2f3df2
Marvin Borner <git@marvinborner.de>
2025-02-13 18:55:51 +0100

    Implement runners for Python and C++

--- generate/reference/are-we-fast-yet.sh
+++ generate/reference/are-we-fast-yet.sh
@@ -1,40 +1,81 @@
 #!/bin/env bash
 set -e
 
-RUNS=5
+# TODO: we could supply a outer and inner loop iteration count to harness.*
 
-# JavaScript benchmarks
-
-cd are-we-fast-yet/benchmarks/JavaScript/
-
+# are-we-fast-yet uses different filenames than we do!
 TRACKED="Bounce List Mandelbrot NBody Permute Queens Sieve Storage Towers"
 RENAMED="bounce list_tail mandelbrot nbody permute queens sieve storage towers"
 PREFIX="are_we_fast_yet"
 
-log="../../../../../effekt/benchmarks_js_reference.log"
->"$log"
+ARR_RENAMED=($RENAMED)
 
+# Hyperfine can only write JSON to files
+tmpfile=$(mktemp /tmp/hyperfine_are-we-fast-yet.XXXXX)
+
+# command -> json
+benchmark() {
+	hyperfine --export-json "$tmpfile" "$1" &>/dev/null
+	jq "{mean: .results[0].mean, stddev: .results[0].stddev}" "$tmpfile"
+}
+
+# output start
+echo "{"
+
+# --- JavaScript benchmarks ---
+
+cd reference/are-we-fast-yet/benchmarks/JavaScript/
+
+echo "\"js\":"
 i=0
-for benchmark in $TRACKED; do
-	total_time=0
-	total_mem=0
-	for run in $(seq $RUNS); do
-		read -r time mem <<<$("$(which time)" --verbose node harness.js "$benchmark" 2>&1 |
-			gawk 'NR==6{time=split($0, arr1, " ")}; match($0, /.*Maximum resident set size \(kbytes\): ([0-9]+)/, arr2){print arr1[3], arr2[1]}')
-
-		time=${time::-2} # remove us suffix
-		total_time=$((total_time + time * 1000))
-		total_mem=$((total_mem + mem))
+{
+	for benchmark in $TRACKED; do
+		echo "{\"$PREFIX/${ARR_RENAMED[$i]}\":"
+		benchmark "node harness.js $benchmark"
+		echo "}"
+		i=$((i + 1))
 	done
+} | jq -s 'add'
 
-	arr=($RENAMED)
-	renamed=${arr[$i]}
+echo ","
 
-	average_time=$((total_time / RUNS))
-	average_mem=$((total_mem / RUNS))
-	echo "$PREFIX/$renamed: 0 $average_time $average_mem" >>"$log"
+# --- Python benchmarks ---
 
-	i=$((i + 1))
-done
+cd ../Python
 
-cd ../../../
+echo "\"python\":"
+i=0
+{
+	for benchmark in $TRACKED; do
+		echo "{\"$PREFIX/${ARR_RENAMED[$i]}\":"
+		benchmark "python harness.py $benchmark"
+		echo "}"
+		i=$((i + 1))
+	done
+} | jq -s 'add'
+
+echo ","
+
+# --- C++ benchmarks ---
+
+cd ../C++
+
+# generated using the build.sh script -- TODO: Do we want to change this?
+clang++ -Wall -Wextra -Wno-unused-private-field -O3 -flto -march=native -ffp-contract=off -std=c++17 src/harness.cpp src/deltablue.cpp src/memory/object_tracker.cpp src/richards.cpp -o harness &>/dev/null
+
+echo "\"cpp\":"
+i=0
+{
+	for benchmark in $TRACKED; do
+		echo "{\"$PREFIX/${ARR_RENAMED[$i]}\":"
+		benchmark "./harness $benchmark"
+		echo "}"
+		i=$((i + 1))
+	done
+} | jq -s 'add'
+
+# --- Done ---
+
+# output end
+echo "}"
+rm "$tmpfile"
