6872f86c788b0e8945b63d78c4a56b2abb53735f
Marvin Borner <git@marvinborner.de>
2025-02-25 12:14:27 +0100

    Only sync references with argument

--- generate/reference.sh
+++ generate/reference.sh
@@ -3,15 +3,15 @@ set -e
 
 >&2 echo "$0"
 
-# generate reference data per request
 if [ "$1" = "sync" ]; then
+	# generate reference data per request
 	rm -f ../data/reference.json
 	{
 		./reference/are-we-fast-yet.sh
 		./reference/duality-of-compilation.sh
 		./reference/effect-handlers-bench.sh
 	} | jq -s 'reduce .[] as $x ({}; . * $x)' >../data/reference.json
+else
+	# effekt data
+	./reference/effekt.sh
 fi
-
-# effekt data
-./reference/effekt.sh
