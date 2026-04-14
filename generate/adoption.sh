78ec63d022461ea0a6246b2c57e020fed6ceebb3
Marvin Borner <git@marvinborner.de>
2026-04-14 14:37:17 +0200

    Fix rate limit on adoption
    optimally we would use the gh-internal blackbird_count api but we can't
    with only a GH_TOKEN, we'd need to generate a session token and mock the
    user agent

--- generate/adoption.sh
+++ generate/adoption.sh
@@ -3,14 +3,19 @@ set -e
 
 >&2 echo "$0"
 
-query='NOT is:fork NOT user:effekt-lang extension:effekt'
+TRIALS=10
+QUERY='NOT is:fork NOT user:effekt-lang extension:effekt'
 
-# one dry run (paginating forces github to find new data, maybe)
-gh api -X GET --paginate search/code -f q="$query" &>/dev/null
-sleep 5
-
-count="$(gh api -X GET search/code -f q="$query" | jq -r '.total_count')"
+# GH api sometimes returns smaller numbers, so we run a few times and take the maximum, lol
+max_count=0
+for _ in $(seq $TRIALS); do
+	count=$(gh api -X GET search/code -f q="$QUERY" -f per_page=1 | jq -r '.total_count')
+	if ((count > max_count)); then
+		max_count=$count
+	fi
+	sleep 6 # 10 requests/min = 1 per 6s
+done
 
 jq -n \
-	--arg count "$count" \
+	--arg count "$max_count" \
 	'{count_github: $count}'
